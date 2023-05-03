61bf0450b64a8e865d7cd9018790fdc0e7b5e461
uoa-epik990 <euanpike@gmail.com>
2023-05-03 17:27:02 +1200

    Update memory_interface.vhd

--- ReCOP/memory_interface.vhd
+++ ReCOP/memory_interface.vhd
@@ -4,6 +4,7 @@ use ieee.std_logic_unsigned.all;
 
 entity memory_interface is
 	port (
+		clock : in std_logic := '0';
 		pc : in std_logic_vector(15 downto 0);
 		rx : in std_logic_vector(15 downto 0);
 		rz : in std_logic_vector(15 downto 0);
@@ -13,15 +14,31 @@ entity memory_interface is
 		address_control : in std_logic_vector (1 downto 0);
 		mem_sel : in std_logic := 'X';
 		
-		address_a : out std_logic_vector(11 downto 0);
-		address_b : out std_logic_vector(11 downto 0);
-		
-		data_b : out std_logic_vector(15 downto 0);
+		program_memory : out std_logic_vector(15 downto 0);
+		data_memory : out std_logic_vector(15 downto 0)
 	);
 end entity memory_interface;
 
 architecture behaviour of memory_interface is
 	signal temp_address : std_logic_vector(15 downto 0);
+	signal address_a : std_logic_vector(11 downto 0);
+	signal address_b : std_logic_vector(11 downto 0);
+	signal data_b : std_logic_vector(15 downto 0);
+	
+	component memory_block
+		port (
+			address_a		: in std_logic_vector (11 DOWNTO 0);
+			address_b		: in std_logic_vector (11 DOWNTO 0);
+			clock		: in std_logic  := '1';
+			data_a		: in std_logic_vector (15 DOWNTO 0);
+			data_b		: in std_logic_vector (15 DOWNTO 0);
+			wren_a		: in std_logic  := '0';
+			wren_b		: in std_logic  := '0';
+			q_a		: out std_logic_vector (15 DOWNTO 0);
+			q_b		: out std_logic_vector (15 DOWNTO 0)
+		);
+	end component memory_block;
+	
 begin
 
 	data_selection : process
@@ -29,6 +46,7 @@ begin
 		case data_in_control is
 			when '0' => data_b <= ir_hold;
 			when '1' => data_b <= rx;
+		end case;
 	end process;
 	
 	address_selection : process
@@ -38,16 +56,31 @@ begin
 			when "01" => temp_address <= rx;
 			when "10" => temp_address <= rz;
 			when "11" => temp_address <= ir_hold;
+		end case;
 	end process;
 	
 	
 	address : process
 	begin
 		if (mel_sel <= '0') then
-			address_a <= temp_address;
+			address_a <= temp_address(11 downto 0);
 		else
-			address_b <= temp_address;
-	end process
+			address_b <= temp_address(11 downto 0);
+		end if;
+	end process;
+	
+	memory_test : memory_block
+		port map (
+			address_a => address_a,
+			address_b => address_b,
+			clock => clock,
+			data_a => open,
+			data_b => data_b,
+			wren_a => open,
+			wren_b => open,
+			q_a => program_memory,
+			q_b => data_memory
+		);
 	
 
 
